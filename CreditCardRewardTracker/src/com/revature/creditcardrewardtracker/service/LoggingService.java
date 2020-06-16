e1fa0a106ea7755e5c6568f8f13083c01b7986f5
Danny <danny.j.eich@gmail.com>
2020-06-16 16:03:22 -0400

    added JUnit Tests

---
+++ CreditCardRewardTracker/src/com/revature/creditcardrewardtracker/service/LoggingService.java
@@ -0,0 +1,22 @@
+package com.revature.creditcardrewardtracker.service;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+//source : https://www.scalyr.com/blog/maven-log4j2-project/
+
+public class LoggingService {
+	
+	/*private static final Logger logger = LogManager.getLogger(LoggingService.class);
+	
+    public static void main( String[] args ) {
+
+		logger.trace("We've just greeted the user!");
+		logger.debug("We've just greeted the user!");
+		logger.info("We've just greeted the user!");
+		logger.warn("We've just greeted the user!");
+		logger.error("We've just greeted the user!");
+		logger.fatal("We've just greeted the user!");
+    }
+    */
+}
